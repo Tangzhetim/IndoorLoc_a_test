fbabca5f5b1311eedb843a7bfd04200634851d61
TimRedd <1093139293@qq.com>
2021-11-09 13:40:34 +0800

    First Update - MOGP ver0.1

--- file_merge.py
+++ file_merge.py
@@ -3,13 +3,13 @@
 # @Author: tangzhetim
 # @File : file_merge.py
 import pandas as pd
-f0 = pd.read_csv('/Users/tangzhe/GitHub/IndoorLoc_a_test/data_silce/validationData2_building0.csv')
 f1 = pd.read_csv('trainingData2_fake.csv')
 f2 = pd.read_csv('/Users/tangzhe/GitHub/IndoorLoc_a_test/data_silce/building0.csv')
 f2.loc[:,"FLOOR"] *= 3
-f0.loc[:,"FLOOR"] *= 3
-flag = pd.concat(f0)
-flag.to_csc("validationData2_building0" + ".csv", index=0, sep=',')
 file = [f1,f2]
 train = pd.concat(file)
 train.to_csv("trainingData2" + ".csv", index=0, sep=',')
+
+# ff = pd.read_csv('/Users/tangzhe/GitHub/IndoorLoc_a_test/data_silce/validationData2.csv')
+# ff.loc[:,"FLOOR"] *=3
+# ff.to_csv("/Users/tangzhe/GitHub/IndoorLoc_a_test/data_silce/validationData2.csv",index=0,sep=',')
